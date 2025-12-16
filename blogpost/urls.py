bbc5ec14e3a20430468071bb28e91a56b52c5b7e
shanhasanova120-ops <shanhasanova120@gmail.com>
2025-12-16 14:24:45 +0400

    Add files via upload

---
+++ blogpost/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from posts.views import homepage, post, about, search, postlist, allposts, tag_list
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', homepage, name = 'homepage'),
+    path('post/<slug>/', post, name = 'post'),
+    path('about/', about,name = 'about' ),
+    path('search/', search, name = 'search'),
+    path('postlist/<slug>/', postlist, name = 'postlist'), 
+    path('posts/', allposts, name = 'allposts'),
+    path('tags/', tag_list, name='tag_list'),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
